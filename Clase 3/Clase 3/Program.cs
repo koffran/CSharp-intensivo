09bc92900c9e1b37055d700bef987f8ead306ecc
Franco Zarate <fran.e.zarate@gmail.com>
2019-02-26 20:56:20 -0300

    Add files via upload

---
+++ Clase 3/Clase 3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Clase_3
+{
+    class Program
+    {
+
+        /*En una pelea entre dos personajes en un juego de Rol uno golpea con un arma 2D6+3 y el
+        otro tiene un escudo 2D4+1, el segundo personaje recibirá un daño igual a la diferencia del
+        golpe del arma con la resistencia del escudo. Si esta resulta negativa o cero, no recibe ningún
+        daño. Hacer un programa que haga las tiradas de los dados y muestre el daño infringido.
+          */
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int d4 = rnd.Next(1, 5), d6 = rnd.Next(1, 7);
+
+            int ataque = 2 * d6 + 3;
+            int defensa = 2 * d4 + 1;
+            int resultado = ataque - defensa;
+
+            int opcion = 0;
+            Console.WriteLine("1- Daño en rol\n2- \n3- n ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    if (ataque > defensa)
+                    {
+                        Console.WriteLine("El ataque es: " + ataque.ToString());
+                        Console.WriteLine("La defensa es: " + defensa.ToString());
+                        Console.WriteLine("El primer personaje saca un " + d6.ToString() +
+                                        "\nEl segundo personaje saca un " + d4.ToString() +
+                                        "\nEl segundo personaje recibe " + resultado.ToString() + " puntos de daño");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El ataque es: " + ataque.ToString());
+                        Console.WriteLine("La defensa es: " + defensa.ToString());
+                        Console.WriteLine("El primer personaje saca un " + d6.ToString() +
+                                        "\nEl segundo personaje saca un " + d4.ToString() +
+                                        "\nEl segundo personaje no recibe daño");
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    Console.WriteLine("Ingresar una opcion correcta <1 - ?> ");
+                    break;
+            }
+
+            Console.WriteLine("\n\nPresione <Escape> para salir del programa");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+        }
+    }
+}
---
+++ Clase 4/Clase 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Clase_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese dos valores");
+            int d1 = int.Parse(Console.ReadLine());
+            int d2 = int.Parse(Console.ReadLine());
+
+           // float p = promedio(d1, d2);
+           
+            Console.WriteLine("El promedio es: " + promedio(d1, d2));
+            int div, resto;
+            divResto(d1, d2, out div, out resto);
+            Console.WriteLine("El dividendo es: " + div + "\nEl resto es: " + resto);
+            Console.ReadKey();
+        }
+
+        public static float promedio(int a, int b)
+        {
+            float retorno = (a + b) / 2.0f;
+            return retorno;
+        }
+
+        static public void divResto(int a, int b,
+                                    out int div, out int resto)
+        {
+            div = a / b;
+            resto = a % b;
+        }
+
+    }
+}
